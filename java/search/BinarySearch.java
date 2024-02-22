d91be9e4529c27fb7a07c62c7441bfae1a0e1135
Gordienko_Konstantin <konstct@niuitmo.ru>
2024-02-22 18:12:58 +0300

    +HW2

---
+++ java/search/BinarySearch.java
@@ -0,0 +1,91 @@
+package search;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static int iterativeBinarySearch(int x, int[] a) {
+        int left = 0;
+        int right = a.length - 1;
+
+        /*
+        forall i in [1, a.length - 1] : a[i - 1] >= a[i]
+        x - integer && immutable
+        {Inv}:
+         Let ind(x): a[ind(x)] <= x
+         right' > left' && left' <= min(ind(x)) <= right':
+             a.length >= 0 && -1 <= min(ind(x)) <= a.length -> true
+        */
+        while (right >= left) {
+            /* {Pred}:
+            left = 0; right = a.length - 1
+            0 <= left' <= right < a.length
+             {Inv} && (right' >= left') -> {Inv}
+            */
+            int mid = left + (right - left) / 2;
+            /*
+                0 <= left <= right < a.length
+                right' >= left' -> right' - left' >= 0;
+                 right' - left' >= 0 && left' >= 0 -> (right' - left') / 2 >= 0;
+                 right' - left' >= 0 - length of the currently considered part of the array ->
+                 -> right' - left' >= (right' - left') / 2 -> left' <= mid' <= right'
+            */
+            if (a[mid] > x) {
+                left = mid + 1;
+                // a[mid' + 1] <= x && a[mid'] > x && {Inv}
+                //New range [left = mid + 1; right] potentially contains desired element
+            } else {
+                right = mid - 1;
+                //a[mid'] <= x && a[mid' - 1] <= x && {Inv}
+            }
+        }
+        // B = {ind: a[ind] <= x} -> left == min(B)
+        return left;
+        //left > right -> !{Inv} - we've checked whole array
+        //{Post}: left > right -> return left
+    }
+
+    public static int recursiveBinarySearch(int x, int[] a, int left, int right) {
+        /*
+        forall i in [1, a.length - 1] : a[i - 1] >= a[i]
+        x - integer && immutable
+        {Inv}: forall i and j in [0; a.length)   0 ≤ i < left : a[i]<x && right < j < len(a) : a[j]>x
+        {Pred}: 0 <= left' <= right < a.length
+        left' + (right' - left') / 2 == (left' + right) / 2;
+        (right' - left') / 2; always: left' <= mid' <= right'
+         */
+        int mid = left + (right - left) / 2;
+        //{Pred}: 0 <= left' <= right < a.length
+        if (left > right) {
+            //left is answer
+            return left;
+            //{Post}: left > right -> return left
+        }
+        if (a[mid] > x) {
+            // a[mid' + 1] <= x && a[mid'] > x && {Inv}
+            return recursiveBinarySearch(x, a, mid + 1, right);
+            //New range [left = mid + 1; right] potentially contains desired element
+        } else {
+            //a[mid'] <= x && a[mid' - 1] <= x && {Inv}
+            return recursiveBinarySearch(x, a, left, mid - 1);
+            //New range [left; right = mid - 1] potentially contains desired element
+        }
+    }
+
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int[] a = new int[args.length - 1];
+        int sum = 0;
+        for (int i = 1; i < args.length; i++) {
+            a[i - 1] = Integer.parseInt(args[i]);
+        }
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        if (sum % 2 == 0) {
+            System.out.println(recursiveBinarySearch(x, a, 0, a.length - 1));
+        } else {
+            System.out.println(iterativeBinarySearch(x, a));
+        }
+    }
+}
---
+++ java/search/BinarySearchClosestA.java
@@ -0,0 +1,115 @@
+package search;
+
+public class BinarySearchClosestA {
+    //{Inv}: x in [a[left'], a[right']]
+    //{Pred}: left = 0; right = a.length - 1;
+    //forall i in [1, a.length - 1] : a[i - 1] <= a[i]
+    //x - integer && immutable
+    public static int iterativeBinarySearch(int x, int[] a) {
+        /*
+        {Inv}:
+         Let A = {abs(x - e) forall e in a} -> ind(x): abs(a[ind(x)] - x) == min(A)
+         right' > left' && left' <= min(ind(x)) <= right':
+             a.length >= 0 && -1 <= min(ind(x)) <= a.length -> true
+        */
+        int left = 0;
+        int right = a.length - 1;
+        while (left <= right) {
+            /* {Pred}:
+            left = 0; right = a.length - 1
+            0 <= left' <= right < a.length
+             {Inv} && (right' >= left') -> {Inv}
+            */
+            int mid = left + (right - left) / 2;
+             /*
+                0 <= left <= right < a.length
+                right' >= left' -> right' - left' >= 0;
+                 right' - left' >= 0 && left' >= 0 -> (right' - left') / 2 >= 0;
+                 right' - left' >= 0 -> right' - left' >= (right' - left') / 2 -> left' <= mid' <= right'
+            */
+            if (a[mid] < x) {
+                //a[mid'] < x && {Inv} -> left' = mid' + 1
+                left = mid + 1;
+                //New range [left = mid + 1; right] potentially contains desired element (element > x)
+            } else if (a[mid] > x) {
+                //a[mid'] > x && {Inv} -> right' = mid' - 1
+                right = mid - 1;
+                //New range [left; right = mid - 1] potentially contains desired element (element < x)
+            } else {
+                return a[mid]; // abs(a[mid] - x) == 0
+            }
+        }
+        //{Post}: left > right -> continue
+        if (left >= a.length) {
+            // x not in a (x > max(a)) -> abs(a[a.length - 1] - x) == min(A)
+            return a[a.length - 1];
+        } else if (left == 0) {
+            // x not in a (x < min(a)) -> abs(a[0] - x) == min(A)
+            return a[0];
+        } else {
+            //min(a) < x < max(a)
+            int closestLess = a[left - 1];
+            int closestSuperior = a[left];
+            /*
+            forall i in [0, a.length - 1] : a[i] == x; min(a) < x < max(a) -> i = left - 1 -- max index such that a[i] < x,
+            а j = left -- min index such that a[j] > x.
+             */
+            return (x - closestLess <= closestSuperior - x) ? closestLess : closestSuperior;
+            // closest to x
+        }
+    }
+
+    private static int recursiveBinarySearch(int[] a, int x, int left, int right) {
+        /*
+        {Inv}:
+         Let A = {abs(x - e) forall e in a} -> ind(x): abs(a[ind(x)] - x) == min(A)
+         right' > left' && left' <= min(ind(x)) <= right':
+             a.length >= 0 && -1 <= min(ind(x)) <= a.length -> true
+        */
+        int mid = left + (right - left) / 2;
+        if (left > right) {
+            //desired element not in a
+            if (right < 0) {
+                // search to the left from the beginning -> closest to x -- a[left'] (0 <= left <= right < a.length)
+                return left;
+            } else if (left >= a.length || x - a[right] <= a[left] - x) {
+                // search was made right from the end -> closest to x -- a[right]
+                return right;
+            } else {
+                //else -- a[left]
+                return left;
+            }
+        }
+        if (a[mid] < x) {
+            // a[mid' + 1] >= x && a[mid'] < x && {Inv}
+            return recursiveBinarySearch(a, x, mid + 1, right);
+            //New range [left = mid + 1; right] potentially contains desired element
+        } else if (a[mid] > x) {
+            //a[mid'] > x && a[mid' - 1] <= x && {Inv}
+            return recursiveBinarySearch(a, x, left, mid - 1);
+            //New range [left; right = mid - 1] potentially contains desired element
+        } else {
+            //a[mid'] == x (closest to x)
+            return mid;
+        }
+        /*
+        in this implementation, we look for an index,
+          and then in the main function we print an array element with a recursiveBinarySearch index (a, x, 0, a.length - 1)
+         */
+    }
+
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int[] a = new int[args.length - 1];
+        int sum = 0;
+        for (int i = 1; i < args.length; i++) {
+            a[i - 1] = Integer.parseInt(args[i]);
+            sum += a[i - 1];
+        }
+        if (sum % 2 == 0) {
+            System.out.println(a[recursiveBinarySearch(a, x, 0, a.length - 1)]);
+        } else {
+            System.out.println(iterativeBinarySearch(x, a));
+        }
+    }
+}
